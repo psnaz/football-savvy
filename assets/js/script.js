76355e0cf9ee18e6282d842a17e347f444175f7e
psnaz <psnazle@gmail.com>
2022-03-09 13:20:47 +0000

    Add event listeners for start, exit and continue button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,5 +3,21 @@ const start_btn = document.querySelector(".start_btn button");
 const info_box = document.querySelector(".info_box");
 const exit_btn = info_box.querySelector(".buttons .quit");
 const continue_btn = info_box.querySelector(".buttons .restart");
+const quiz_box = document.querySelector(".quiz_box");
 
-//
+//If Start Quiz Button clicked
+start_btn.onclick = ()=>{
+    info_box.classList.add("activeInfo"); //display the info box
+}
+
+//If Exit Button clicked
+exit_btn.onclick = ()=>{
+    info_box.classList.remove("activeInfo"); //hide the info box
+}
+
+//If Continue Button clicked
+continue_btn.onclick = ()=>{
+    info_box.classList.remove("activeInfo"); //hide the info box
+    quiz_box.classList.add("activeQuiz"); //display the quiz box
+
+}
