2d1832d3fa924c037fe1da45cf4d6c8ec0fe07ab
psnaz <psnazle@gmail.com>
2022-03-09 15:36:07 +0000

    Create array for question numbers, questions, answer options and correct answers

---
+++ assets/js/questions.js
@@ -0,0 +1,58 @@
+//Create an array for question numbers, questions, answer options and correct answers
+let question = [
+    {
+        numb: 1,
+        question: "He's the 2nd best football player in history, he's Argentinian and ruined his career because of drugs. Who is he?",
+        answer: "Diego Maradona",
+        options: [
+            "Diego Maradona",
+            "Pelé",
+            "Johann Criff",
+            "Adrian Mutu",
+        ]
+    },
+    {
+        numb: 2,
+        question: "He's English, played with Frank Lampard for the National Team and is one of the best Liverpool players ever. Who is he?",
+        answer: "Steven Gerrard",
+        options: [
+            "Wayne Rooney",
+            "Steven Gerrard",
+            "Joe Hard",
+            "Glen Johnson",
+        ]
+    },
+    {
+        numb: 3,
+        question: "He's from Belgium, was one of the most amazing players for Chelsea, but he hasn't played that well for Real Madrid. Who is he?",
+        answer: "Eden Hazard",
+        options: [
+            "Romelu Lukaku",
+            "Thorgan Hazard",
+            "Jan Vertronghen",
+            "Eden Hazard",
+        ]
+    },
+    {
+        numb: 4,
+        question: "He's from France, he's young and has very heavy links to Real Madrid. Who is he?",
+        answer: "Kylian Mbappé",
+        options: [
+            "Ousmane Dembélé",
+            "Kingsley Coman",
+            "Kylian Mbappé",
+            "Karim Benzema",
+        ]
+    },
+    {
+        numb: 5,
+        question: "He is Italian, came through the Academy of AC Milan, he never left the club and retired there. Who is he?",
+        answer: "Paolo Maldini",
+        options: [
+            "Gianni Rivera",
+            "Roberto Baggio",
+            "Paolo Maldini",
+            "Gianluigi Buffon",
+        ]
+    },
+];
