e7fa4fe3cb0ea42cbbcdc8ac176ba364b009e20b
psnaz <psnazle@gmail.com>
2022-03-09 12:25:39 +0000

    Amend html and css classes: change from hypens to underscore in line with the Code Nepal original code for better clarity

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+//all required elements and events
+const start_btn = document.querySelector(".start_btn button");
+const info_box = document.querySelector(".info_box");
+const exit_btn = info_box.querySelector(".buttons .quit");
+const continue_btn = info_box.querySelector(".buttons .restart");
+
+//
